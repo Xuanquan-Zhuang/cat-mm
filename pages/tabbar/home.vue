a9daf607de73752a5ce3d82841dcf34f59dd679a
xiximiao <messiah_chong@hotmail.com>
2021-08-10 01:15:38 +0800

    add pages

---
+++ pages/tabbar/home.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="content">
+		<view class="top-box">
+			
+			<view class="search-box">
+				<image class="search-icon" src="../../static/gallery_search_icon.jpeg"></icon>
+				<input class="search-input" placeholder="搜索感兴趣的关键字" />
+			</view>
+			
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	
+	.top-box {
+		width: 100%;
+		height: 50rpx;
+		display: flex;
+		justify-content: row;
+		align-items: center;
+		margin-top: 10rpx;
+	}
+	
+	.search-box {
+		width: 60%;
+		height: 50rpx;
+		border: 2rpx solid;
+		border-color: #808080;
+		border-radius: 25rpx;
+		margin-left: 2%;
+		display: flex;
+		justify-content: row;
+		align-items: center;
+	}
+	
+	.search-icon {
+		margin-left: 2%;
+		width: 40rpx;
+		height: 40rpx;
+	}
+	
+	.search-input {
+		margin-left: 2%;
+	}
+</style>
